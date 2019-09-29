fcf8978779ed4a68d4becbed7916dfeca2b056e7
Fabrício <fabricio-gf@hotmail.com>
2019-09-29 18:23:38 -0300

    Added ending controller

---
+++ Assets/Scripts/EndingController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingController : MonoBehaviour
+{
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void StartEnding()
+    {
+        animator.SetTrigger("OpenEnding");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    } 
+
+    public void MuteGame()
+    {
+        if (Input.GetKeyDown(KeyCode.M)){
+            AudioManager.instance.MuteAll();
+        }
+    }
+}
